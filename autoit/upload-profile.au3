49608d30001a8d426f883677cb1d7807fb4e2723
kavyasree <kavyasreedoddam@gmail.com>
2023-12-17 19:20:07 +0530

    added files

---
+++ autoit/upload-profile.au3
@@ -0,0 +1,3 @@
+ControlFocus("Downloads", "", "DirectUIHWND3");
+ControlSetText("Downloads", "", "DirectUIHWND3","C:\Users\KAVYA\Downloads\smiley.png");
+ControlClick("Downloads", "", "Button1");
---
+++ src/main/java/com/simplylearn/seleniumtest/Login.java
@@ -0,0 +1,7 @@
+package com.simplylearn.seleniumtest;
+
+public class Login {
+	public static final String username = "kavyasreedoddam@gmail.com";
+	public static final String password = "Kavyadeepu@1426";
+
+}
